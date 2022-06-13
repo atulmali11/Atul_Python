9760ed4b8e866019e782730c06cafa94825787e5
Devu-Pari <maliatulpandharpur@gmail.com>
2022-06-13 21:13:26 +0530

    string operation

---
+++ stringrevdemo.py
@@ -0,0 +1,58 @@
+
+#1
+str="I am Atul Mali"
+
+j=len(str)-1
+rev=" "
+for i in str:
+    rev=i+rev
+print(rev)
+
+#2
+str1="ATUL"
+output=" "
+output=str1[::-1]
+print(output)
+
+#3
+str3="Mali"
+s1=reversed(str3)
+output1=''.join(s1)
+print(output1)
+
+#4
+str4="ATUL MALI BHOSE"
+re=''
+a=len(str4)-1
+
+while a>=0:
+    re=re+str4[a]
+    a=a-1
+print(re)
+
+
+# count of lower and upper character
+str5="I Am Atul Mali"
+lower=0
+upper=0
+i=len(str5)
+for i in str5:
+    if(i.islower()):
+        lower=lower+1
+    elif(i.isupper()):
+        upper=upper+1
+print(upper)
+print(lower)
+
+
+#second way
+str6="AAAbbbCCdd"
+lo=0
+up=0
+for s in str6:
+    if(s>='a' and s<='z'):
+        lo=lo+1
+    elif(s>='A' and s<='Z'):
+        up=up+1
+print(lo)
+print(up)
