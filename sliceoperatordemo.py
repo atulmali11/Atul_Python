8b8b2f1e7dbb31d1b9d9ce1da33f3c0a128f5723
Devu-Pari <maliatulpandharpur@gmail.com>
2022-06-15 06:12:47 +0530

    string operation-slicing

---
+++ sliceoperatordemo.py
@@ -0,0 +1,19 @@
+
+
+str="atul"
+#print(str)
+#print(str[2:3])    #a=0,t=1,u=2,l=3
+#print(str[1:])
+#print(str[::])
+#print(str[:3])
+#print(str[2:])
+str1=str[:1].upper()
+print(str1)
+str2=str[1:3]
+print(str2)
+str3=str[3:4].upper()
+print(str3)
+result=str1+str2+str3
+print(result)
+
+
