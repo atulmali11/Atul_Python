f4bf79016a5b010193b885567674bef8af7560d9
Devu-Pari <maliatulpandharpur@gmail.com>
2022-06-19 06:26:43 +0530

    greenkart demo

--- PythonSelenium/greencartdemo1.py
+++ PythonSelenium/greencartdemo1.py
@@ -1,16 +1,13 @@
-#Impicit wait
-#Explicit wait
-#using time class-->provide by python
 import time
-from logging import exception
 
 from selenium import webdriver
-from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.support.wait import WebDriverWait
 
+
+lst=[]
+lst1=[]
 driver=webdriver.Chrome(executable_path="D:\\Python_Atul\\chromedriver\\chromedriver.exe")
+driver.implicitly_wait(5)
 driver.get("https://rahulshettyacademy.com/seleniumPractise/#/")
 driver.maximize_window()
 driver.find_element(by=By.CSS_SELECTOR,value="input.search-keyword").send_keys("ber")
@@ -18,18 +15,28 @@ time.sleep(3)
 count=len(driver.find_elements(by=By.XPATH,value="//div[@class='products']/div"))
 #print(len(count))
 assert count==3
+
 buttons=driver.find_elements(by=By.XPATH,value="//div[@class='product-action']/button")
+
 for button in buttons:
+    lst.append(button.find_element(by=By.XPATH,value="parent::div/parent::div/h4").text)
     button.click()
+print(lst)
+
+
 driver.find_element(by=By.CSS_SELECTOR,value="img[alt='Cart']").click()
 driver.find_element(by=By.XPATH,value="//button[text()='PROCEED TO CHECKOUT']").click()
-wait=WebDriverWait(driver,4)
-wait.until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR,'.promocode')))
+
+original_amount=driver.find_element(by=By.CSS_SELECTOR,value=".discountAmt").text
 driver.find_element(by=By.CSS_SELECTOR,value="input.promocode").send_keys("rahulshettyacademy")
 driver.find_element(by=By.CSS_SELECTOR,value=".promoBtn").click()
-wait.until(expected_conditions.presence_of_element_located((By.XPATH,"//span[text()='Code applied ..!']")))
 promocode=driver.find_element(by=By.CSS_SELECTOR,value=".promoInfo").text
 print(promocode)
+discounted_amount= driver.find_element(by=By.CSS_SELECTOR,value=".discountAmt").text
+names=driver.find_elements(by=By.CSS_SELECTOR,value="p.product-name")
+for veg in names:
+    lst1.append(veg.text)
+print(lst1)
+assert lst==lst1
 
-
-
+assert float(discounted_amount)<int(original_amount)
---
+++ dictdemo1.py
@@ -0,0 +1,14 @@
+
+
+dict={'Name':'Atul', 'Lname':'Mali', 'Name':'Suresh','Lname':'Mali'}
+
+print(len(dict))
+print(dict)
+print(dict.copy())
+
+y=0
+print(dict.fromkeys(dict,y))
+
+print(dict.get('Name'))
+
+print(dict.items())
